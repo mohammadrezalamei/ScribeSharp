fb96e59bb317a4786d9b5ce7bd636aea2f54672e
Mohammad Reza Lamei <me@mohammadrezalamei.com>
2025-04-03 00:57:54 +0330

    Start

---
+++ ScribeSharp.Form/Forms/Main.cs
@@ -0,0 +1,10 @@
+﻿namespace ScribeSharp.Form.Forms
+{
+    public partial class Main : System.Windows.Forms.Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ScribeSharp.Form/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ScribeSharp.Form
+{
+    internal static class Program
+    {
+        [System.STAThread]
+        static void Main()
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            ScribeSharp.Form.Forms.Main mainForm = 
+                new();
+            System.Windows.Forms.Application.Run(mainForm);
+        }
+    }
+}
